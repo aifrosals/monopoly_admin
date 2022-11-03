c9c2f25279341d3c6447cd0cd9967508f6f03d92
Bryson Jones <jbrysondev05@gmail.com>
2022-11-03 18:42:07 -0400

    working on reset_password_provider.dart

--- lib/web/admin/admin_app.dart
+++ lib/web/admin/admin_app.dart
@@ -14,7 +14,7 @@ import 'package:provider/provider.dart';
 
 class AdminApp extends StatelessWidget {
   const AdminApp({Key? key}) : super(key: key);
-// admi
+// admin
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
